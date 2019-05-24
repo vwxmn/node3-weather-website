9de69a7ed3228fdbba11b16f68ef810b7a87f5bb
vwxmn <50995393+vwxmn@users.noreply.github.com>
2019-05-25 01:57:59 +0800

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,8 +9,8 @@ const forecast = (longitude, latitude, callback) => {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature +' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        } else {        
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature +' degrees out. The high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
